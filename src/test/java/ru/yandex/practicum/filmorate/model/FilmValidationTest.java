51d70a48749107328a74f671b5266c3d51bd27cc
Yaroslav Tamliani <yaroslav.tamliane@ya.ru>
2025-11-15 15:27:30 +0700

    Исправил тесты

--- src/test/java/ru/yandex/practicum/filmorate/model/FilmValidationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/model/FilmValidationTest.java
@@ -47,7 +47,7 @@ public class FilmValidationTest {
         Set<ConstraintViolation<Film>> violations = validator.validate(film, OnCreate.class);
 
         assertFalse(violations.isEmpty());
-        assertEquals(7, violations.size(), "Ожидалось 7 ошибок");
+        assertEquals(6, violations.size(), "Ожидалось 6 ошибок");
     }
 
     @Test
