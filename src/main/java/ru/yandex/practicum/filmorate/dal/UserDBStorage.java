95fd731cd74b237c3f817a786ac0cb0d4085818d
kumis <dxdkymis@mail.ru>
2025-12-10 22:31:27 +0300

    Add fix: delete user - delete relations

--- src/main/java/ru/yandex/practicum/filmorate/dal/UserDBStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/UserDBStorage.java
@@ -19,6 +19,8 @@ public class UserDBStorage extends BaseRepository<User> implements UserStorage {
     private static final String INSERT_QUERY = "INSERT INTO USERS (EMAIL, LOGIN, NAME, BIRTHDAY) " + "VALUES (?, ?, ?, ?)";
     private static final String UPDATE_QUERY = "UPDATE USERS SET EMAIL = ?, LOGIN = ?, NAME = ?, BIRTHDAY = ? " + "WHERE USER_ID = ?";
     private static final String DELETE_QUERY = "DELETE FROM USERS WHERE USER_ID = ?";
+    private static final String DELETE_USER_FRIENDSHIPS = "DELETE FROM FRIENDSHIP WHERE USER_ID = ? AND FRIEND_ID = ?";
+    private static final String DELETE_USER_LIKES = "DELETE FROM FILM_LIKES WHERE USER_ID = ?";
 
     public UserDBStorage(JdbcTemplate jdbc, RowMapper<User> mapper) {
         super(jdbc, mapper);
@@ -90,6 +92,11 @@ public class UserDBStorage extends BaseRepository<User> implements UserStorage {
     public User delete(Long id) {
         User user = findById(id)
                 .orElseThrow(() -> new NoSuchElementException("Пользователь не найден"));
+
+        // удаляем и связи пользователя при его удалении
+        jdbc.update(DELETE_USER_FRIENDSHIPS, id, id); // здесь user_id и friend_id
+        jdbc.update(DELETE_USER_LIKES, id); // здесь user_id
+
         delete(DELETE_QUERY, id);
         return user;
     }
