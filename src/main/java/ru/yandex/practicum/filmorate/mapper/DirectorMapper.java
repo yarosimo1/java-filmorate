8c5e1cf53d6fb49c4b146b14b72b05c7706cefb0
ropgocTb <216320907+ropgocTb@users.noreply.github.com>
2025-12-14 23:59:54 +0300

    Added: create/update DTO's for director entity

--- src/main/java/ru/yandex/practicum/filmorate/mapper/DirectorMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mapper/DirectorMapper.java
@@ -2,7 +2,9 @@ package ru.yandex.practicum.filmorate.mapper;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import ru.yandex.practicum.filmorate.dto.director.DirectorCreateDto;
 import ru.yandex.practicum.filmorate.dto.director.DirectorDto;
+import ru.yandex.practicum.filmorate.dto.director.DirectorUpdateDto;
 import ru.yandex.practicum.filmorate.model.Director;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -14,10 +16,36 @@ public final class DirectorMapper {
                 .build();
     }
 
+    public static DirectorDto mapToDto(DirectorCreateDto directorCreateDto) {
+        return DirectorDto.builder()
+                .name(directorCreateDto.getName())
+                .build();
+    }
+
+    public static DirectorDto mapToDto(DirectorUpdateDto directorUpdateDto) {
+        return DirectorDto.builder()
+                .id(directorUpdateDto.getId())
+                .name(directorUpdateDto.getName())
+                .build();
+    }
+
     public static Director mapToDirector(DirectorDto directorDto) {
         return Director.builder()
                 .id(directorDto.getId())
                 .name(directorDto.getName())
+                .build();
+    }
+
+    public static Director mapToDirector(DirectorCreateDto directorCreateDto) {
+        return Director.builder()
+                .name(directorCreateDto.getName())
+                .build();
+    }
+
+    public static Director mapToDirector(DirectorUpdateDto directorUpdateDto) {
+        return Director.builder()
+                .id(directorUpdateDto.getId())
+                .name(directorUpdateDto.getName())
                 .build();
     }
 }
