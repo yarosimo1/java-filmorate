349480d2fe89426202dfb1d1c5efb1bc31bc75b0
ropgocTb <216320907+ropgocTb@users.noreply.github.com>
2025-12-16 14:26:59 +0300

    minor format changes

--- src/main/java/ru/yandex/practicum/filmorate/exception/ConditionsNotMetExceptions.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exception;
-
-public class ConditionsNotMetExceptions extends RuntimeException {
-    public ConditionsNotMetExceptions(String message) {
-        super(message);
-    }
-}
